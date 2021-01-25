cdd3a6d652a8f137d33ca1d1e2a4c46f7f8c9fe7
Etienne XPS <etiennetran@hotmal.fr>
2021-01-25 16:27:19 +0100

    correction warning

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,24 +9,24 @@ export default class Footer extends Component {
                     <div className="footer-content">
                         <a href="#">
                             <div className="small-icon">
-                                <i class="fa fa-facebook" aria-hidden="true"></i>
+                                <i className="fa fa-facebook" aria-hidden="true"></i>
                             </div>
                         </a>
                         <a href="#">
                             <div className="small-icon">
-                                <i class="fa fa-linkedin" aria-hidden="true"></i>
+                                <i className="fa fa-linkedin" aria-hidden="true"></i>
                             </div>
                         </a>
                         <a href="#">
                             <div className="small-icon">
-                                <i class="fa fa-instagram" aria-hidden="true"></i>
+                                <i className="fa fa-instagram" aria-hidden="true"></i>
                             </div>
                         </a>
                     </div>
                 </div>
                 <div className="footer-container">
                     <div className="footer-content">
-                        <i class="fa fa-copyright">Copyright</i>
+                        <i className="fa fa-copyright">Copyright</i>
                     </div>
                 </div>
             </footer>
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 import './Navigation.css';
 
 export default class Navigation extends Component {
+
+    handleClick = () => {
+        console.log("gérer le clique");
+    }
+
     render() {
         return (
             <div className="navigation">
@@ -12,8 +17,8 @@ export default class Navigation extends Component {
                     <li><a className="responsive-link" href="#">Github</a></li>
                     <li><a className="responsive-link" href="#">Contact</a></li>
                     <li>
-                        <a href="javascript:void(0);" class="nav-icon" onclick="functionAFaire()">
-                            <i class="fa fa-bars fa-w-14 fa-3x"></i>
+                        <a href="#" className="nav-icon" onClick={this.handleClick}>
+                            <i className="fa fa-bars fa-w-14 fa-3x"></i>
                         </a>
                     </li>
                 </ul>
