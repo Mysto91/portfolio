9f2fdfa105b29c7c4f1e398de6a0b93b9d8c8967
Etienne TRAN <etiennetran@hotmail.fr>
2021-01-27 13:41:28 +0100

    amélioration hover + transformation fonction js en react

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -6,24 +6,23 @@ export default class Navigation extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            navClass: 'navigation'
+            topNavClass: 'topnav'
         };
     }
 
     handleClick = (event) => {
-        var x = document.getElementById("myTopnav");
-        if (x.className === "topnav") {
-            x.className += " responsive";
-        } else {
-            x.className = "topnav";
-        }
-
+        this.setState((state) => {
+            if (state.topNavClass === 'topnav')
+                return { topNavClass: state.topNavClass + ' responsive' };
+            else
+                return { topNavClass: 'topnav' };
+        })
     }
 
     render() {
         return (
-            <div id="nav-bar" className={this.state.navClass}>
-                <div className="topnav" id="myTopnav">
+            <div id="nav-bar" className="navigation">
+                <div className={this.state.topNavClass} id="myTopnav">
                     <a href="#home" className="active">Présentation</a>
                     <a href="#news">Parcours</a>
                     <a href="#contact">Création</a>
