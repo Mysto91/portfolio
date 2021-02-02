c52a25dabe1b99e77e5e9837d31d2512c2ee8369
Etienne TRAN <etiennetran@hotmail.fr>
2021-02-02 17:57:13 +0100

    Changement de nom menulink

--- src/data/data.js
+++ src/data/data.js
@@ -3,4 +3,4 @@ import { getCardList, getMenuLinkList, getRowList } from '../util/function.js'
 export const experienceCardList = getCardList(require('./json/experience.json'))
 export const formationCardList = getCardList(require('./json/school.json'))
 export const skillLanguageList = getRowList(require('./json/skillLanguage.json'))
-export const menuLinkList = getMenuLinkList(require('./json/link.json'))
+export const menuLinkList = getMenuLinkList(require('./json/menuLink.json'))
