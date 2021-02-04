2b00ae2ba4f43511d010fe4db66a0a3ca5ad8f1d
Etienne TRAN <etiennetran@hotmail.fr>
2021-02-04 12:14:39 +0100

    Ajout smooth rotation sur arrow responsive

--- src/components/dropListContainer/DropListContainer.js
+++ src/components/dropListContainer/DropListContainer.js
@@ -7,12 +7,14 @@ export default class DropListContainer extends Component {
         super(props);
 
         this.state = {
-            listClass: 'list'
+            listClass: 'list',
+            arrowBtnClass: 'fa fa-angle-down fa-w-10 fa-5x drop-list'
         }
     }
 
     handleClickDropList = (event) => {
-        this.setState(state => state.listClass === 'list' ? { listClass: state.listClass + ' responsive' } : { listClass: 'list' })
+        this.setState(state => state.listClass === 'list' ? { listClass: state.listClass + ' responsive' } : { listClass: 'list' });
+        this.setState(state => state.arrowBtnClass.includes('arrow-open') ? { arrowBtnClass: state.arrowBtnClass.replace('arrow-open', '') } : { arrowBtnClass: state.arrowBtnClass + ' arrow-open' });
     }
 
     render() {
@@ -22,7 +24,7 @@ export default class DropListContainer extends Component {
         return (
             <div className='drop-list-container'>
                 <h2>{title}</h2>
-                <div className='fa fa-angle-down fa-w-10 fa-5x drop-list' onClick={this.handleClickDropList} />
+                <div className={this.state.arrowBtnClass} onClick={this.handleClickDropList} />
                 <div className={this.state.listClass}>
                     {
                         content
